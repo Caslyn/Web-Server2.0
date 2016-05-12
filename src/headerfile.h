c435d1871b1aea3147c94d27cd03e88890143b06
Caslyn <caslyn.cole@gmail.com>
2016-05-12 16:52:13 -0700

    flush out serverside connection_handler

--- src/headerfile.h
+++ src/headerfile.h
@@ -2,3 +2,8 @@
 int get_address(void);
 int connect_client(char *, int *);
 int client_recv(int *);
+
+/* server.c */
+int get_address(void);
+int connect_server(int *);
+int accept_connection(int *);
