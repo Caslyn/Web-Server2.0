499111b256a4facb4f657b36cecba76a95aedaa5
Caslyn <caslyn.cole@gmail.com>
2016-05-11 17:36:42 -0700

    put client code into single file to execute client connection easily

--- src/client.c
+++ src/client.c
@@ -19,6 +19,33 @@
 
 static struct addrinfo hints, *servinfo, *p;
 
+/* initiate client connection
+  * expect client to provide hostname to connect to
+ */
+
+int main(int argc, char *argv[]) {
+  int rv, client_sockfd;
+  char s[INET6_ADDRSTRLEN];
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: client hostname\n");
+    return 1;
+  }
+
+  if ((rv = get_address()) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return 1;
+  }
+
+  if (connect_client(s, &client_sockfd) != 0) {
+    fprintf(stderr, "client: failed to connect\n");
+    return 1;
+  } else {
+    printf("client: connecting to %s\n", s);
+  }
+  client_recv(&client_sockfd);
+}
+
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa) {
   if(sa->sa_family == AF_INET) {
